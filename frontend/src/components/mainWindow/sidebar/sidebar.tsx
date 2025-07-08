b6fb6208822a20fe37a21a9431ede6b9df14e095
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-07-08 19:44:48 +0900

    Increase responsiveness of the sidebar

--- frontend/src/components/mainWindow/sidebar/sidebar.tsx
+++ frontend/src/components/mainWindow/sidebar/sidebar.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { type Tab } from "../../../types";
-import {
-  House,
-  FileAudio,
-  Captions,
-  Settings,
-  type LucideIcon,
-} from "lucide-react";
+import { House, FileAudio, Captions, Settings, type LucideIcon } from "lucide-react";
 
 interface SidebarProps {
   currentTab: Tab;
@@ -21,16 +15,18 @@ const Sidebar: React.FC<SidebarProps> = ({ currentTab, onTabChange }) => {
     {key: 'settings',    label: 'Settings',    icon: Settings},
   ];
   return (
-    <div className="bg-[rgb(15,15,15)]">
+    <div className="bg-[rgb(20,20,20)]">
       {tabs.map((tab) => {
         const Icon = tab.icon;
         return (
           <div
             key={tab.key}
             onClick={() => onTabChange(tab.key)}
-            className={`flex items-center gap-2 py-2 ps-2 pe-5 
-            hover:bg-[rgb(0,0,0)] cursor-pointer
-            ${currentTab === tab.key ? "bg-[rgb(0,0,0)]" : "bg-[rgb(15,15,15)]"}
+            className={`flex items-center gap-2 mx-2 my-0.5 py-1.5 ps-4 pe-6 
+            rounded-[15px] cursor-pointer 
+            transition-all duration-300 transform
+            hover:bg-[rgb(40,40,40)] hover:translate-x-5
+            ${currentTab === tab.key ? "bg-[rgb(40,40,40)]" : "bg-[rgb(20,20,20)]"}
           `}
           >
             <Icon size={25} strokeWidth={1} className="text-white"></Icon>
